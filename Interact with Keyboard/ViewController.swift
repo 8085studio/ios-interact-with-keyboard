9fb72361108ae384f002cff671151d0b69dbb893
Herman Liang <hermanliang80@gmail.com>
2015-06-14 09:14:51 +0800

    Initial Commit

---
+++ Interact with Keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Interact with Keyboard
+//
+//  Created by Herman Liang on 2015/6/14.
+//  Copyright (c) 2015年 Herman Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
