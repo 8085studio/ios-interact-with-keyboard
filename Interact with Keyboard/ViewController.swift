488d9356fe8905b228e544ad0eefbb96d596f6ef
Herman Liang <herman@wfe.com.tw>
2015-07-16 13:50:22 +0800

    interact with bottom text field

--- Interact with Keyboard/ViewController.swift
+++ Interact with Keyboard/ViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak private var textBottom: UITextField!
+    private var currentTextField: UITextField?
+    private var isKeyboardShown = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "keyboardWillShow:",
+            name: UIKeyboardWillShowNotification,
+            object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "keyboardWillHide:",
+            name: UIKeyboardWillHideNotification,
+            object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        currentTextField = textField
+    }
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        
+        return true
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
@@ -27,6 +50,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         self.view.endEditing(true)
+    }
+    
+    func keyboardWillShow(note: NSNotification) {
+        if isKeyboardShown {
+            return
+        }
+        if (currentTextField != textBottom) {
+            return
+        }
+        let keyboardAnimationDetail = note.userInfo as! [String: AnyObject]
+        let duration = NSTimeInterval(keyboardAnimationDetail[UIKeyboardAnimationDurationUserInfoKey]! as! NSNumber)
+        let keyboardFrameValue = keyboardAnimationDetail[UIKeyboardFrameBeginUserInfoKey]! as! NSValue
+        let keyboardFrame = keyboardFrameValue.CGRectValue()
+        
+        UIView.animateWithDuration(duration, animations: { () -> Void in
+            self.view.frame = CGRectOffset(self.view.frame, 0, -keyboardFrame.size.height)
+        })
+        isKeyboardShown = true
+    }
+    
+    func keyboardWillHide(note: NSNotification) {
+        let keyboardAnimationDetail = note.userInfo as! [String: AnyObject]
+        let duration = NSTimeInterval(keyboardAnimationDetail[UIKeyboardAnimationDurationUserInfoKey]! as! NSNumber)
+        UIView.animateWithDuration(duration, animations: { () -> Void in
+            self.view.frame = CGRectOffset(self.view.frame, 0, -self.view.frame.origin.y)
+        })
+        isKeyboardShown = false
     }
 
 }
